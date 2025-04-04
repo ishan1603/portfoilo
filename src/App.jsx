611ac5e5e34feddd9df2dda942d3bbd66cdeb8f4
Ishan <ishanm1603@gmail.com>
2025-04-05 01:57:29 +0530

    edit hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <main>
-      <h1 className=" text-5xl text-bold text-orange-500 text-">
-        Welcome to portfolio
-      </h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   );
 };
