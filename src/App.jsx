d674281e73630db72b95cd760ba419b61ff97095
Ishan <ishanm1603@gmail.com>
2025-04-04 22:27:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className=" text-5xl text-bold text-orange-500 text-">
+        Welcome to portfolio
+      </h1>
+    </main>
+  );
+};
+
+export default App;
